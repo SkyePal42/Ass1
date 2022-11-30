db2176ffd65b3ca2236cb6cae7a03da9b0019c63
Skye Carroll <skyecarroll42@gmail.com>
2022-11-30 21:18:03 +0000

    moving things around

--- server.js
+++ server.js
@@ -3,15 +3,27 @@ const app = express();
 
 const port = 8090;
 app.use(express.static(__dirname + "/assets"));
+app.use(express.static(__dirname + "/client"));
 
-app.get(["/", "/index", "/index.html", "home"], (req, res) => {
-	res.sendFile(__dirname + "/index.html");
+app.get(["/", "/index", "/home"], (req, res) => {
+	res.sendFile(__dirname + "/client/index.html");
 });
 
-app.get("", (req, res) => {});
+app.get("/post/get/all", (req, res) => {
+	resp.json({
+		data: "hello",
+	});
+});
+
+app.get("/list", function (req, resp) {
+	resp.json({
+		data: "hello",
+		second_data: "hello2",
+	});
+});
 
 app.get("/*", (req, res) => {
-	res.sendFile(__dirname + "/404.html");
+	res.sendFile(__dirname + "/client/404.html");
 });
 
 app.listen(port, () => {
