97fd90d3c3e84fa00a022a3e31fb04bbe261a052
Skye Carroll <skyecarroll42@gmail.com>
2022-12-01 17:17:05 +0000

    fontawesome package used, dynamic like detection

--- server.js
+++ server.js
@@ -2,31 +2,42 @@
 
 const express = require("express");
 const app = express();
+const jsonfile = require("jsonfile");
 
 const port = 8090;
 app.use(express.static(__dirname + "/assets"));
 app.use(express.static(__dirname + "/client"));
+app.use(express.static(__dirname + "/node_modules/@fortawesome/fontawesome-free"));
 
 app.get(["/", "/index", "/home"], (req, res) => {
 	res.sendFile(__dirname + "/client/index.html");
 });
 
-app.get("/api/post/get/all", (req, res) => {
-	// TODO https://stackabuse.com/reading-and-writing-json-files-with-node-js/
-	res.json([
-		{
-			id: 0,
-			title: "To the Mun",
-			content:
-				"We plan to go to the Mun. Not because it is easy, but because its easier than Minmus.",
-			user: "Kerman",
-			creationDate: 1669906884089,
-			lastEdit: 1669906884089,
-			likes: 0,
-			liked: false,
-			comments: 0,
-		},
-	]);
+app.get("/api/post/get/all/:user", (req, res) => {
+	let user = req.params.user;
+
+	const file = __dirname + "/data.json";
+	var data;
+	jsonfile.readFile(file, (err, data) => {
+		if (err) console.error(err);
+
+		let posts = [];
+		data.posts.forEach((post, index) => {
+			posts.push({
+				id: index,
+				title: post.title,
+				content: post.content,
+				user: post.user,
+				creationDate: post.creationDate,
+				lastEdit: post.lastEdit,
+				likes: post.likes.length,
+				liked: post.likes.includes(user),
+				comments: post.comments.length,
+			});
+		});
+
+		res.json(posts);
+	});
 });
 
 app.get("/list", function (req, resp) {
