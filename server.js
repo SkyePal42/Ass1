565ae8b643eead48226877a00af50239ac43e142
Skye Carroll <skyecarroll42@gmail.com>
2022-12-01 23:55:14 +0000

    can like and add new post

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const app = express();
 const jsonfile = require("jsonfile");
 
 const port = 8090;
+app.use(express.json());
 app.use(express.static(__dirname + "/assets"));
 app.use(express.static(__dirname + "/client"));
 app.use(express.static(__dirname + "/node_modules/@fortawesome/fontawesome-free"));
@@ -17,7 +18,6 @@ app.get("/api/post/get/all/:user", (req, res) => {
 	let user = req.params.user;
 
 	const file = __dirname + "/data.json";
-	var data;
 	jsonfile.readFile(file, (err, data) => {
 		if (err) console.error(err);
 
@@ -38,6 +38,65 @@ app.get("/api/post/get/all/:user", (req, res) => {
 
 		res.json(posts);
 	});
+});
+
+function removeItemOnce(arr, value) {
+	var index = arr.indexOf(value);
+	if (index > -1) {
+		arr.splice(index, 1);
+	}
+	return arr;
+}
+
+app.post("/api/post/like", (req, res) => {
+	const file = __dirname + "/data.json";
+	jsonfile.readFile(file, (err, data) => {
+		if (err) console.error(err);
+		if (req.body.post < data.posts.length) {
+			if ("user" in req.body && req.body.user) {
+				let post = data.posts[req.body.post];
+				if (post.likes.includes(req.body.user)) {
+					post.likes = removeItemOnce(post.likes, req.body.user);
+				} else {
+					post.likes.push(req.body.user);
+				}
+				res.json({
+					status: 200,
+					liked: post.likes.includes(req.body.user),
+					likes: post.likes.length,
+				});
+				data.posts[req.body.post] = post;
+				jsonfile.writeFile(file, data, function (err) {
+					if (err) console.error(err);
+				});
+			} else {
+				res.json({ status: 400, message: "Invalid Username" });
+			}
+		} else {
+			res.json({ status: 404, message: "Array OutOfBounds" });
+		}
+	});
+});
+
+app.post("/api/post/add", (req, res) => {
+	const file = __dirname + "/data.json";
+	jsonfile.readFile(file, (err, data) => {
+		if (err) console.error(err);
+		data.posts.push({
+			title: req.body.title,
+			content: req.body.content,
+			user: req.body.user,
+			creationDate: Date.now(),
+			lastEdit: Date.now(),
+			likes: [],
+			comments: [],
+		});
+		jsonfile.writeFile(file, data, function (err) {
+			if (err) console.error(err);
+		});
+	});
+
+	res.sendStatus(200);
 });
 
 app.get("/list", function (req, resp) {
